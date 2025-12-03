0f32a668bd531eaf59fb44c83fbba3332600ffc0
Moondust <ellybau2001@gmail.com>
2025-12-03 16:03:56 +0100

    setup H2

--- src/main/java/com/agora/Secure_Notes_Service/SecureNotesServiceApplication.java
+++ src/main/java/com/agora/Secure_Notes_Service/SecureNotesServiceApplication.java
@@ -12,7 +12,7 @@ public class SecureNotesServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecureNotesServiceApplication.class, args);
 	}
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
