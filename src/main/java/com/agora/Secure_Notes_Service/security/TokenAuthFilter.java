4eca857fc6662843c73c1cfd2ed8bf9e3d74a5ff
Moondust <ellybau2001@gmail.com>
2025-12-05 15:37:24 +0100

    Token-based authentication implemented

---
+++ src/main/java/com/agora/Secure_Notes_Service/security/TokenAuthFilter.java
@@ -0,0 +1,49 @@
+package com.agora.Secure_Notes_Service.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TokenAuthFilter extends OncePerRequestFilter {
+
+    @Value("${notes.api.token}")
+    private String validToken;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        System.out.println("VALID TOKEN: ");
+        System.out.println(validToken);
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            if (request.getRequestURI().startsWith("/h2-console")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Missing Authorization header");
+            return;
+        }
+
+        String token = authHeader.substring(7); // Remove "Bearer "
+
+        if (!token.equals(validToken)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid token");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
